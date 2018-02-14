c25d1160fefc677bd7f6c0aef47cf8cf4a3dbeb7
ederpbrito <eder.brito@resourceit.com>
2018-02-13 22:14:18 -0200

    Inclusão de tratamento de excessões

---
+++ src/main/java/br/com/fiap/petShow/exceptions/ControllerAdvice.java
@@ -0,0 +1,16 @@
+package br.com.fiap.petShow.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+public class ControllerAdvice {
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public String handleException (Exception exception, Model model) {
+        model.addAttribute("exception", exception);
+        return "exception-page";
+    }
+}
